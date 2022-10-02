6c42fc223542d450d55ef2792c5557cd5b4111d5
mansakondo <mansakondo22@gmail.com>
2022-10-02 13:20:42 +0200

    Update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :events
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "home#index"
+  root "events#index"
 end
