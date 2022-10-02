27a822e2c496896340f3a240b302dbd694d1aa26
mansakondo <mansakondo22@gmail.com>
2022-10-02 13:36:03 +0200

    Rename the app to `stimulus-rephlex-calendar`

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails7StimulusReflexEsbuild
+module StimulusRephlexCalendar
   class Application < Rails::Application
     config.autoload_paths << "#{root}/app"
     # Initialize configuration defaults for originally generated Rails version.
